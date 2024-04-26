a3fb8c7df5a1cb56ba7907d6bff1c71bdf8f2c7d
Duy Vu <1205vuduy2001@gmail.com>
2024-04-26 22:35:58 +0300

    refactor

--- detr_resnet_50.py
+++ detr_resnet_50.py
@@ -3,7 +3,7 @@ import torch
 from PIL import Image
 import requests
 
-def object_tracking(img):
+def object_tracking_detr_resnet(img):
     # you can specify the revision tag if you don't want the timm dependency
     processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
     model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
@@ -15,6 +15,7 @@ def object_tracking(img):
     # let's only keep detections with score > 0.9
     target_sizes = torch.tensor([image.size[::-1]])
     results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+    print("Results' keys:", results)
 
     for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
         box = [round(i, 2) for i in box.tolist()]
@@ -23,7 +24,9 @@ def object_tracking(img):
                 f"{round(score.item(), 3)} at location {box}"
         )
 
+
+
 if __name__ == "__main__":
     url = "http://images.cocodataset.org/val2017/000000039769.jpg"
     image = Image.open(requests.get(url, stream=True).raw)
-    object_tracking(image)
+    object_tracking_detr_resnet(image)
--- object_tracking_script.py
+++ object_tracking_script.py
@@ -1,13 +1,12 @@
-# import the opencv library 
 import cv2
+from detr_resnet_50 import object_tracking_detr_resnet
 
-video_path = "../video_17s.mp4"
+video_path = "../video_data/video_17s.mp4"
 vid = cv2.VideoCapture(video_path) 
 
 while(True):
 	ret, frame = vid.read()
-	
-
+	object_tracking_detr_resnet()
 	cv2.imshow('frame', frame) 
 	
 	if cv2.waitKey(1) & 0xFF == ord('q'): 
