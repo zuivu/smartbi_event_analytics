9d830e6264bc4ac3dc3b1a5fdb3e36154cb9d582
Minh Nguyen <quangminh240924@gmail.com>
2024-04-27 03:40:19 +0300

    fix: get corrected object class name

--- get_object_list.py
+++ get_object_list.py
@@ -1,13 +1,13 @@
-def get_object_list(frames):
+def get_object_list(frames, names):
     person_list = {}
     object_list = {}
     for results in frames:
         boxes = results[0].boxes.xywh.cpu()
         track_ids = results[0].boxes.id.int().cpu().tolist()
-        object_classes = results[0].boxes.cls.str().cpu().tolist()
+        class_ids = results[0].boxes.cls.int().cpu().tolist()
 
-        for box, track_id, object_class in zip(boxes, track_ids, object_classes):
-            if object_class == "Person":
+        for box, track_id, class_id in zip(boxes, track_ids, class_ids):
+            if names[class_id] == "Person":
                 add_to_dict(box, track_id, person_list)
             else:
                 add_to_dict(box, track_id, object_list)
